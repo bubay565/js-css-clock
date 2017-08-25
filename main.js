20220338300e5fab570f5f8a3da5374c3419cbe2
Buwa <bubay565@yahoo.com>
2017-08-25 06:29:51 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const secondHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector(`.min-hand`);
+const hourHand = document.querySelector('.hour-hand');
+function setDate(){
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // added 90 to make up for offset in css
+    
+    const minDegrees = ((minutes / 60) * 360) + 90;
+    
+    const hourDegrees = ((hours / 12) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
